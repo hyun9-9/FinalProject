6cbad11b2f4cce3353df3c3cb7f1f5fd641d56f3
박현구 <pwc1088@naver.com>
2023-05-25 14:49:49 +0900

    search clear

--- app/src/main/java/com/example/finalproject/SearchFragment1.java
+++ app/src/main/java/com/example/finalproject/SearchFragment1.java
@@ -31,10 +31,10 @@ import java.util.List;
  * Use the {@link SearchFragment1#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SearchFragment1 extends Fragment{
-//    SettingList settingList;
+public class SearchFragment1 extends Fragment {
+    //    SettingList settingList;
     private List<String> list;          // 데이터를 넣은 리스트변수
-    Context context ;
+    Context context;
 
 
     //private ListView listView;          // 검색을 보여줄 리스트변수
@@ -43,7 +43,7 @@ public class SearchFragment1 extends Fragment{
     private EditText editSearch;        // 검색어를 입력할 Input 창
     private SearchAdapter adapter;      // 리스트뷰에 연결할 아답터
 
-   private ArrayList<String> arraylist;
+    private ArrayList<String> arraylist;
 
     private SearchView searchView;
 
@@ -90,9 +90,9 @@ public class SearchFragment1 extends Fragment{
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View frag_view = inflater.inflate(R.layout.fragment_search,container,false);
+        View frag_view = inflater.inflate(R.layout.fragment_search, container, false);
         //editSearch = (EditText) frag_view.findViewById(R.id.editSearch);
         //listView = (ListView) frag_view.findViewById(R.id.listView);
         Bundle bundle = getArguments();
@@ -100,73 +100,48 @@ public class SearchFragment1 extends Fragment{
         list = new ArrayList<String>();
 
         gridView = (GridView) frag_view.findViewById(R.id.gridView);
-        searchView=frag_view.findViewById(R.id.search);
-        context=container.getContext();
+        searchView = frag_view.findViewById(R.id.search);
+        context = container.getContext();
 
 
-        Response.Listener<String> responseListener = new Response.Listener<String>() {
+        settingList();
+        arraylist = new ArrayList<String>();
+        arraylist.addAll(list);
+        adapter = new SearchAdapter(list, container.getContext());
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonObject = new JSONObject( response );
-
-                    Log.e("Test123",jsonObject.toString());
-                    JSONArray data_json=jsonObject.getJSONArray("data");
-                    Log.e("Test123",data_json.toString());
-                    for(int i=0;i<data_json.length();i++){
-                        // jsonObject1=data_json.getJSONObject(i);
-                        //Log.e("Test123",jsonObject1.toString());
-                        list.add(data_json.getString(i));
-                        Log.e("name",data_json.getString(i));
-                    }
-
-                    //settingList();
-                    arraylist = new ArrayList<String>();
-                    arraylist.addAll(list);
-                    adapter = new SearchAdapter(list,container.getContext());
-                    gridView.setAdapter(adapter);
-                    gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 //                                Log.d(TAG, "여기야 : "+l_id.get(position));
 //                                Intent intent =new Intent(getActivity(),ProductActivity.class);
 //                                intent.putExtra("id",l_id.get(position));
 //                                startActivity(intent);
-                        }
-                    });
-
-                    searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                        @Override
-                        public boolean onQueryTextSubmit(String query) {
-
-
-                            Log.d(TAG,"rrrrrrrrrrrrrrrrrrrrrr");
-                            return false;
-                        }
-
-                        @Override
-                        public boolean onQueryTextChange(String newText) {
-
-                            search(newText);
-                            return false;
-                        }
-                    });
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
             }
-        };
+        });
 
-        SearchRequest searchRequest = new SearchRequest( access_token, responseListener );
-        RequestQueue queue = Volley.newRequestQueue( frag_view.getContext() );
-        queue.add( searchRequest );
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+
+                Log.d(TAG, "rrrrrrrrrrrrrrrrrrrrrr");
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                search(newText);
+                return false;
+            }
+        });
 
 
         return frag_view;
 
 
     }
+
     // 검색을 수행하는 메소드
     public void search(String charText) {
 
@@ -178,14 +153,11 @@ public class SearchFragment1 extends Fragment{
             list.addAll(arraylist);
         }
         // 문자 입력을 할때..
-        else
-        {
+        else {
 //            // 리스트의 모든 데이터를 검색한다.
-            for(int i = 0;i < arraylist.size(); i++)
-            {
+            for (int i = 0; i < arraylist.size(); i++) {
 //                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
-                if (arraylist.get(i).toLowerCase().contains(charText))
-                {
+                if (arraylist.get(i).toLowerCase().contains(charText)) {
 //                    // 검색된 데이터를 리스트에 추가한다.
 
                     list.add(arraylist.get(i));
@@ -196,25 +168,11 @@ public class SearchFragment1 extends Fragment{
         // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
         adapter.notifyDataSetChanged();
     }
+
     // 검색에 사용될 데이터를 리스트에 추가한다.
-    private void settingList(){
-        //settingList = new SettingList(context);
-
-
-        //list = settingList.getlist();
-//        l_brand.addAll(list.get(0));
-//        l_name.addAll(list.get(1));
-//        l_price.addAll(list.get(2));
-//        l_image.addAll(list.get(3));
-
-
-
-
-       // l_id.addAll(settingList.l_id);
-       // l_brand.addAll(settingList.l_brand);
-       // l_name.addAll(settingList.l_name);
-      //  l_price.addAll(settingList.l_price);
-       //  l_image.addAll(settingList.l_image);
+    private void settingList() {
+        SearchRepository dao = SearchRepository.getInstance();
+        list.addAll(dao.getAllProducts());
 
     }
 }
